4fc940e7dd2e1b5da42f504e81bf1619f970a14b
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2026-02-14 16:27:08 -0300

    feat: criacao do grip de corretas vs ativos

--- src/components/Ativos.tsx
+++ src/components/Ativos.tsx
@@ -1,24 +1,56 @@
 export default function Ativos() {
-  
-
+  const ativos = [
+    {
+      nome: 'XP Investimentos',
+      ativos: 73,
+      variacao: '37,87%',
+      percentualCarteira: '37,87%',
+    },
+    {
+      nome: 'Rico',
+      ativos: 37,
+      variacao: '37,87%',
+      percentualCarteira: '37,87%',
+    },
+    {
+      nome: 'NU Investimentos',
+      ativos: 23,
+      variacao: '37,87%',
+      percentualCarteira: '37,87%',
+    },
+    {
+      nome: 'Inter',
+      ativos: 7,
+      variacao: '37,87%',
+      percentualCarteira: '37,87%',
+    },
+  ]
 
   return (
     <div className="flex flex-col gap-4 p-4">
-      <p>Corretoras vs Ativos</p>
-      <div className="flex flex-col justify-around gap-4">
-        <p className="font-bold">XP Investimentos</p>
-        <div className="flex gap-2 justify-between">
-          <p>Ativos</p>
-          <p className="font-bold">37</p>
+      <p className="text-lg">Corretoras vs Ativos</p>
+
+      {ativos.map((ativo) => (
+        <div key={ativo.nome}>
+          <p className="font-bold">{ativo.nome}</p>
+          <div className="flex gap-2 justify-between">
+            <p>Ativos</p>
+            <p className="font-bold">{ativo.ativos}</p>
         </div>
         <div className="flex gap-2 justify-between">
           <p>Variação</p>
-          <p className="font-bold">37,87%</p>
+          <p className="font-bold">{ativo.variacao}</p>
         </div>
         <div className="flex gap-2 justify-between">
           <p>% na carteira</p>
-          <p className="font-bold">37,87%</p>
+          <p className="font-bold">{ativo.percentualCarteira}</p>
         </div>
+      </div>  
+      ))}
+
+      <div className="flex gap-2 justify-between pt-4">
+        <p className="font-bold">Total</p>
+        <p>{ativos.reduce((total, ativo) => total + ativo.ativos, 0)}</p>
       </div>
     </div>
   )
