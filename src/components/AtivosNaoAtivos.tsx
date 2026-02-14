291656b15f036c7ac2ea393bb8b10d9fc32c7928
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2026-02-14 17:16:58 -0300

    feat: criando componente dinanmico

--- src/components/AtivosNaoAtivos.tsx
+++ src/components/AtivosNaoAtivos.tsx
@@ -1,30 +1,42 @@
 export default function AtivosNaoAtivos({ className }: { className?: string }) {
+  const ativos = [
+    {
+      ano: 2026,
+      salario: 13878.24,
+      contasReceber: 415.38,
+      investimentos: 200000.00,
+      reservaEmergencia: 32123.85,
+    }
+  ]
+
   return (
     <section className={`flex flex-col gap-4 p-4 bg-white rounded-lg shadow-lg ${className}`}>
       <p className="text-lg">Ativos vs Não Ativos</p>
 
-      <article className="flex flex-col gap-1">
-        <p className="font-medium">2026</p>
-        <div className="flex justify-between">
-          <p>Salário</p>
-          <p className="font-medium">R$ 13.878,24</p>
-        </div>
+      {ativos.map((ativo) => (
+        <article className="flex flex-col gap-1">
+          <p className="font-medium">{ativo.ano}</p>
+          <div className="flex justify-between">
+            <p>Salário</p>
+            <p className="font-medium">R$ {ativo.salario.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</p>
+          </div>
 
-        <div className="flex justify-between">
-          <p>Contas á receber</p>
-          <p className="font-medium">R$ 415,38</p>
-        </div>
+          <div className="flex justify-between">
+            <p>Contas á receber</p>
+            <p className="font-medium">R$ {ativo.contasReceber.toLocaleString('pt-BR', { minimumFractionDigits: 2 })} </p>
+          </div>
 
-        <div className="flex justify-between">
-          <p>Investimentos</p>
-          <p className="font-medium">R$ 200.000,00</p>
-        </div>
+          <div className="flex justify-between">
+            <p>Investimentos</p>
+            <p className="font-medium">R$ {ativo.investimentos.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</p>
+          </div>
 
-        <div className="flex justify-between">
-          <p>Reserva de emergência</p>
-          <p className="font-medium">R$ 32.123,85</p>
-        </div>
-      </article>
+          <div className="flex justify-between">
+            <p>Reserva de emergência</p>
+            <p className="font-medium">R$ {ativo.reservaEmergencia.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}   </p>
+          </div>
+        </article>
+      ))}
     </section>
   );
 };
