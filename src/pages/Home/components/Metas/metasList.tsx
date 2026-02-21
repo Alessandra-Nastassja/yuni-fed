8e5eb03068d8232a2d824863c8bb9d3660ea5856
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2026-02-21 00:44:10 -0300

    feat: movendo a regra para o back

--- src/pages/Home/components/Metas/metasList.tsx
+++ src/pages/Home/components/Metas/metasList.tsx
@@ -15,13 +15,7 @@ export default function MetasList({ metas, onAddClick }: MetasListProps) {
   return (
     <>
       {metas && metas.length > 0 ? (
-        // TODO: fazer ordenação por percentual de alcance da meta (valorAtual / valorMeta) no back
         metas
-        .sort((a, b) => {
-          const pa = a.valorMeta ? (Number(a.valorAtual ?? 0) / Number(a.valorMeta)) * 100 : 0
-          const pb = b.valorMeta ? (Number(b.valorAtual ?? 0) / Number(b.valorMeta)) * 100 : 0
-          return pa - pb
-        })
         .map((meta, index) => (
           <section key={index} className="flex flex-col justify-between gap-2">
             <div className='flex justify-between gap-5'>
